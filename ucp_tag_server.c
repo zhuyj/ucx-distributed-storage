6e73446717dd88425586eeb1774e0d320c07b870
Zhu Yanjun <yanjunz@mellanox.com>
2020-02-12 04:49:17 +0200

    ucx-tag: move the communication from main loop to the callback function
    Now the callback function will handle the communication.

--- ucp_tag_server.c
+++ ucp_tag_server.c
@@ -23,6 +23,7 @@ static uint16_t server_port = DEFAULT_PORT;
 typedef struct ucx_server_ctx {
     volatile ucp_conn_request_h conn_request;
     ucp_listener_h              listener;
+    ucp_context_h               ucp_context;
 } ucx_server_ctx_t;
 
 /**
@@ -345,6 +346,10 @@ static int init_worker(ucp_context_h ucp_context, ucp_worker_h *ucp_worker)
     return ret;
 }
 
+static ucs_status_t server_create_ep(ucp_worker_h data_worker,
+                                     ucp_conn_request_h conn_request,
+                                     ucp_ep_h *server_ep);
+
 /**
  * The callback on the server side which is invoked upon receiving a connection
  * request from the client.
@@ -355,7 +360,28 @@ static void server_conn_handle_cb(ucp_conn_request_h conn_request, void *arg)
     ucs_status_t status;
 
     if (context->conn_request == NULL) {
+        ucp_worker_h     ucp_data_worker;
+        ucp_ep_h         server_ep;
+        int              ret;
+        ucs_status_t     status;
+
         context->conn_request = conn_request;
+        ret = init_worker(context->ucp_context, &ucp_data_worker);
+        if (ret != 0)
+            goto err;
+
+        status = server_create_ep(ucp_data_worker, context->conn_request, &server_ep);
+        if (status != UCS_OK) {
+            ret = -1;
+            goto err_worker;
+        }
+
+        client_server_communication(ucp_data_worker, server_ep);
+
+err_worker:
+        ucp_worker_destroy(ucp_data_worker);
+err:
+        context->conn_request = NULL;
     } else {
         /* The server is already handling a connection request from a client,
          * reject this new one */
@@ -449,20 +475,13 @@ static int run_server(ucp_context_h ucp_context, ucp_worker_h ucp_worker,
                       char *listen_addr)
 {
     ucx_server_ctx_t context;
-    ucp_worker_h     ucp_data_worker;
-    ucp_ep_h         server_ep;
     ucs_status_t     status;
     int              ret;
 
-    /* Create a data worker (to be used for data exchange between the server
-     * and the client after the connection between them was established) */
-    ret = init_worker(ucp_context, &ucp_data_worker);
-    if (ret != 0) {
-        goto err;
-    }
-
     /* Initialiaze the server's context. */
     context.conn_request = NULL;
+
+    memcpy(&context.ucp_context, &ucp_context, sizeof(ucp_context_h));
 
     /* Create a listener on the worker created at first. The 'connection
      * worker' - used for connection establishment between client and server.
@@ -480,34 +499,12 @@ static int run_server(ucp_context_h ucp_context, ucp_worker_h ucp_worker,
          * If there are multiple clients for which the server's connection request
          * callback is involked, i.e. several clients are trying to connect in
          * parallel, the server will handle only the first one and reject the rest */
-        while (context.conn_request == NULL) {
             ucp_worker_progress(ucp_worker);
-        }
-
-        /* Server creates an ep to the client on the data worker.
-         * This is not the worker the listener was created on.
-         * The client side should have initiated the connection, leading
-         * to this ep's creation */
-        status = server_create_ep(ucp_data_worker, context.conn_request,
-                                  &server_ep);
-        if (status != UCS_OK) {
-            ret = -1;
-            goto err_listener;
-        }
-
-        client_server_communication(ucp_data_worker, server_ep);
-
-        /* Reinitialize the server's context to be used for the next client */
-        context.conn_request = NULL;
-
-        printf("Waiting for connection...\n");
     }
 
-err_listener:
     ucp_listener_destroy(context.listener);
+
 err_worker:
-    ucp_worker_destroy(ucp_data_worker);
-err:
     return ret;
 }
 
